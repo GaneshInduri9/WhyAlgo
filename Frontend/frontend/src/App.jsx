42e935a98ec546e6053bbe01438654c4c4bfa129
Ganesh Induri <ganeshinduri4@gmail.com>
2025-03-02 20:47:39 +0530

    add logo and navbar [wip]
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

---
+++ Frontend/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-3">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
